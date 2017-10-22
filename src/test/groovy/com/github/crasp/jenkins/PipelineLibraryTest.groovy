45d9bfe9ed3c3f600b8cf95bf58dbdf46192d4ed
Christian Rasp <christian.rasp@gmail.com>
2017-10-22 11:47:39 +0200

    Create the same setup for all test cases.

--- src/test/groovy/com/github/crasp/jenkins/PipelineLibraryTest.groovy
+++ src/test/groovy/com/github/crasp/jenkins/PipelineLibraryTest.groovy
@@ -1,29 +1,24 @@
-package com.github.crasp.pipeline.unit
+package com.github.crasp.jenkins
 
 import com.lesfurets.jenkins.unit.BasePipelineTest
 import org.junit.Before
-import org.junit.Test
 
 import static com.lesfurets.jenkins.unit.global.lib.LibraryConfiguration.library
 import static com.lesfurets.jenkins.unit.global.lib.LocalSource.localSource
 
-class TestExampleJob extends BasePipelineTest {
+abstract class PipelineLibraryTest extends BasePipelineTest {
 
     @Override
     @Before
+    /**
+     * Do the same setup for all derived test cases in order to use the same library definition everywhere.
+     */
     void setUp() throws Exception {
         super.setUp()
-        helper.registerSharedLibrary(library('jenkins-library')
+        helper.registerSharedLibrary(library('library-under-test')
             .defaultVersion('master')
             .targetPath('target')
             .retriever(localSource('target'))
             .build())
-    }
-
-    @Test
-    void should_execute_without_errors() throws Exception {
-        runScript("src/test/resources/exampleJob.pipeline")
-        printCallStack()
-        assertJobStatusSuccess()
     }
 }

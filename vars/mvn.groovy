650d0fb2e1f1f6bbb7e13c19dbae17f8c1569ce9
Christian Rasp <christian.rasp@gmail.com>
2017-10-24 14:05:01 +0200

    Remove redundant parameter evaluation and mixups.

--- vars/mvn.groovy
+++ vars/mvn.groovy
@@ -1,9 +1,19 @@
 def call(parameters = [:]) {
     String goals = parameters.containsKey('goals') ? parameters.goals : 'clean install'
-    String options = parameters.containsKey('mavenOpts') ? parameters.mavenOpts : ''
-    String properties = parameters.containsKey('properties') ? parameters.properties : ''
+    String options = parameters.containsKey('options') ? parameters.options : ''
 
-    options += "${options.isEmpty() ? '' : ' '}--update-snapshots --batch-mode"
+    List<String> optionList = []
+    if (!options.isEmpty()) {
+        optionList.add(options)
+    }
 
-    sh "mvn ${goals} ${options}${properties.isEmpty() ? '' : " ${properties}"}"
+    // Make sure these options are always active when running on Jenkins
+    if (!(options.contains('-U') || options.contains('--update-snapshots'))) {
+        optionList.add('--update-snapshots')
+    }
+    if (!(options.contains('-B') || options.contains('--batch-mode'))) {
+        optionList.add('--batch-mode')
+    }
+
+    sh "mvn ${goals} ${optionList.join(' ')}"
 }

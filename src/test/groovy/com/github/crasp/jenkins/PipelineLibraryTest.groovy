42d417897322a2eb4e16f35ffaac27f90a7aa864
Christian Rasp <christian.rasp@gmail.com>
2017-10-26 20:08:24 +0200

    Refactor helper stuff into abstract class.

--- src/test/groovy/com/github/crasp/jenkins/PipelineLibraryTest.groovy
+++ src/test/groovy/com/github/crasp/jenkins/PipelineLibraryTest.groovy
@@ -6,7 +6,6 @@ import org.junit.Rule
 import org.junit.rules.TestName
 
 import static com.lesfurets.jenkins.unit.global.lib.LibraryConfiguration.library
-import static PipelineLibraryTestHelper.createTempScript
 import static com.lesfurets.jenkins.unit.global.lib.LocalSource.localSource
 
 abstract class PipelineLibraryTest extends BasePipelineTest {
@@ -21,6 +20,12 @@ abstract class PipelineLibraryTest extends BasePipelineTest {
      */
     void setUp() throws Exception {
         super.setUp()
+
+        registerLibrary()
+        registerMethods()
+    }
+
+    private void registerLibrary() {
         helper.registerSharedLibrary(library('library-under-test')
             .defaultVersion('master')
             .targetPath('target')
@@ -28,7 +33,38 @@ abstract class PipelineLibraryTest extends BasePipelineTest {
             .build())
     }
 
+    private void registerMethods() {
+        helper.registerAllowedMethod('someFunnyStuff', [Map.class, Closure.class], null)
+    }
+
     protected void callMethod(final String method) {
-        runScript(createTempScript(testName.getMethodName(),method))
+        runScript(createTempScript(method))
+    }
+
+    /**
+     * Create a script file containing the whole library definition stuff around the given method call.
+     */
+    private String createTempScript(final String methodCall) {
+        File outputDir = new File("target/debug-vars")
+        if (!outputDir.exists()) {
+            outputDir.mkdirs()
+        }
+        File file = new File(outputDir, "${getClass().getSimpleName()} - ${testName.getMethodName()}.pipeline")
+        file.newWriter().withWriter { writer ->
+            writer << "@Library('library-under-test') _\n"
+            writer << "node() {\n"
+            writer << "    stage('testMe') {\n"
+            writer << "        ${methodCall}\n"
+            writer << "    }\n"
+            writer << "}\n"
+        }
+        return file.getPath()
+    }
+
+    /**
+     * Collect all shell arguments in the current call stack.
+     */
+    protected String getShellArguments() {
+        return helper.callStack.findAll { 'sh' == it.methodName }.collect { it.argsToString() }.join(',')
     }
 }

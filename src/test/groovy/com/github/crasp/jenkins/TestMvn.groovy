f0210b1057722fe011b7832ff94d77ac6c12c678
Christian Rasp <christian.rasp@gmail.com>
2017-10-26 20:08:44 +0200

    Adjust and expand tests.

--- src/test/groovy/com/github/crasp/jenkins/TestMvn.groovy
+++ src/test/groovy/com/github/crasp/jenkins/TestMvn.groovy
@@ -2,7 +2,6 @@ package com.github.crasp.jenkins
 
 import org.junit.Test
 
-import static PipelineLibraryTestHelper.collectShellArguments
 import static org.junit.Assert.assertEquals
 
 class TestMvn extends PipelineLibraryTest {
@@ -12,8 +11,7 @@ class TestMvn extends PipelineLibraryTest {
 
         callMethod('mvn()')
 
-        String actual = collectShellArguments(helper.callStack)
-        assertEquals('mvn clean install --update-snapshots --batch-mode', actual)
+        assertEquals('mvn clean install --update-snapshots --batch-mode', getShellArguments())
         assertJobStatusSuccess()
     }
 
@@ -22,8 +20,43 @@ class TestMvn extends PipelineLibraryTest {
 
         callMethod("mvn(goals: 'validate')")
 
-        String actual = collectShellArguments(helper.callStack)
-        assertEquals('mvn validate --update-snapshots --batch-mode', actual)
+        assertEquals('mvn validate --update-snapshots --batch-mode', getShellArguments())
+        assertJobStatusSuccess()
+    }
+
+    @Test
+    void 'should not override short snapshot update option'() throws Exception {
+
+        callMethod("mvn(options: '-U')")
+
+        assertEquals('mvn clean install -U --batch-mode', getShellArguments())
+        assertJobStatusSuccess()
+    }
+
+    @Test
+    void 'should not override short batch mode option'() throws Exception {
+
+        callMethod("mvn(options: '-B')")
+
+        assertEquals('mvn clean install -B --update-snapshots', getShellArguments())
+        assertJobStatusSuccess()
+    }
+
+    @Test
+    void 'should not override snapshot update option'() throws Exception {
+
+        callMethod("mvn(options: '--update-snapshots -DskipTests=false')")
+
+        assertEquals('mvn clean install --update-snapshots -DskipTests=false --batch-mode', getShellArguments())
+        assertJobStatusSuccess()
+    }
+
+    @Test
+    void 'should not override batch mode option'() throws Exception {
+
+        callMethod("mvn(options: '--batch-mode -DskipTests=false')")
+
+        assertEquals('mvn clean install --batch-mode -DskipTests=false --update-snapshots', getShellArguments())
         assertJobStatusSuccess()
     }
 }
